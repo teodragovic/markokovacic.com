06cf2f3825ac6d8180ab5ba116f475e9bc22ea71
Teo Dragovic <teo.dragovic@gmail.com>
2018-10-02 19:16:56 +0200

    Add backup

---
+++ _backup/mms/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 2,
+ 
+  });
+
+  $(".youtube").fitVids();
+ 
+});
